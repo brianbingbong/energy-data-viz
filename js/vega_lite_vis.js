378973b4b960f8e69e8366b7c144c1ca1311876e
Brian Li Ong <bong0005@student.monash.edu>
2021-09-21 15:12:32 +1000

    Improve directory stucture
    Made directories for css files, data files and js files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
 var vg_1 = "vega-chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
